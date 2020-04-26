3d62eefe661d8692ae9e53be510fdd5109b9324e
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2020-04-26 19:41:41 +1000

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,55 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        
+        //cordova specific events
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('deviceready', this.testMethod.bind(this), false);
+
+        document.addEventListener('pause', this.pauseListener.bind(this), false);
+
+        document.addEventListener('resume', this.resumeListener.bind(this), false); 
+        
+
+        //application events
+
+        
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    testMethod: function(){
+        alert("TESTING! :O");
+    },
+    
+    pauseListener: function(){
+        // alert("ON PAUSE");
+        // save state here when user switches app
+    },
+
+    resumeListener: function(){
+        // Resume the saved state and reload the UI with the saved data
+        // alert("ON RESUME");
+    },
+
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
